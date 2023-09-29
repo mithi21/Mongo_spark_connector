526c9e4b0e52afa0e2aaad4219872f2c8dc3d0e1
Chirag R Patel <mithitagadiya@gmail.com>
2023-09-29 02:58:58 -0400

    Update spark_mongo_Test.py

--- spark_mongo_Test.py
+++ spark_mongo_Test.py
@@ -1,5 +1,16 @@
-
+#bin/spark-shell --conf "spark.driver.extraJavaOptions=-Dhttp.proxyHost=<proxyHost> -Dhttp.proxyPort=<proxyPort> -Dhttps.proxyHost=<proxyHost> -Dhttps.proxyPort=<proxyPort>" --packages <somePackage>
+#https://stackoverflow.com/questions/36676395/how-to-resolve-external-packages-with-spark-shell-when-behind-a-corporate-proxy
+#https://stackoverflow.com/questions/36676395/how-to-resolve-external-packages-with-spark-shell-when-behind-a-corporate-proxy
+#proxyPort=<proxyPort> -Dhttps.proxyHost=<proxyHost> -Dhttps.proxyPort=<proxyPort>
+#to $SPARK_HOME/conf/spark-defaults.conf works for me.
+#https://rangareddy.github.io/SparkSubmitProxyConfiguration/
+#https://docs.informatica.com/integration-cloud/data-integration/current-version/transformations/machine-learning-transformation/configuring-an-api-proxy/configuring-a-spark-proxy.html
+#https://docs.cloudera.com/machine-learning/saas/spark/topics/ml-proxy-setup.html
+#https://docs.cloudera.com/machine-learning/saas/spark/topics/ml-proxy-setup.html
+#https://sparkbyexamples.com/spark/spark-set-jvm-options-to-driver-executors/
 import pyspark
+
+
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, SQLContext, functions as F
 from pyspark.sql.functions import *
